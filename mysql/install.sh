79072d51d22e0aec506666e014d33c037db86103
Maxim Kraev <maxim.kraev@gmail.com>
2016-04-26 11:13:22 +0100

    try to use bash instead of sh

--- mysql/install.sh
+++ mysql/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sh -c "$(curl -fsSL https://raw.github.com/codio/install_software/master/tools/ansible.sh)" mysql
+bash -c "$(curl -fsSL https://raw.github.com/codio/install_software/master/tools/ansible.sh)" mysql
 
 echo "Mysql password root user password is 'codio'"
--- python2/install.sh
+++ python2/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sh -c "$(curl -fsSL https://raw.github.com/codio/install_software/master/tools/ansible.sh)" python2
+bash -c "$(curl -fsSL https://raw.github.com/codio/install_software/master/tools/ansible.sh)" python2
