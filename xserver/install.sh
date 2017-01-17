658c71a7204b85d53d76ff86815cfde1e529c2a8
Vasiliy Levykin <levykinv+codiodex7@gmail.com>
2017-01-17 14:39:30 +0000

    Update preview targets after installing the X server.
    https://codio.myjetbrains.com/youtrack/issue/codio-8165

--- xserver/install.sh
+++ xserver/install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 bash -c "$(curl -fsSL https://raw.github.com/codio/install_software/master/tools/ansible.sh)" xserver
+
+echo
+echo "A Virtual Desktop item has been added automatically to the Preview menu in the menu bar above to allow you access X-server."
+echo "For more information on this see https://codio.com/docs/ide/boxes/installsw/gui/"
