a632b7d2545cd2b2673141603efa9185a78e25a6
Maxim Kraev <maxim.kraev@gmail.com>
2017-04-28 11:47:20 +0100

    xserver requires restart warning

--- xserver/install.sh
+++ xserver/install.sh
@@ -3,5 +3,9 @@
 bash -c "$(curl -fsSL https://raw.github.com/codio/install_software/master/tools/ansible.sh)" xserver
 
 echo
+echo "******************************************************************************************************"
+echo "*    YOU MUST NOW RESTART YOUR BOX FROM THE MENU Project-Restart Box.                                *" 
+echo "******************************************************************************************************"
+echo
 echo "A Virtual Desktop item has been added automatically to the Preview menu in the menu bar above to allow you access X-server."
 echo "For more information on this see https://codio.com/docs/ide/boxes/installsw/gui/"
