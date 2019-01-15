5f4666a5d46484e394062816d676a21c9c75e7e6
Dmitrii Suchkov <dsuchkov@codio.com>
2019-01-15 12:54:41 +0300

    fix install software ansible scripts for ubuntu 16.06 and 18.04 (#19)
    * fix install software ansible scripts for ubuntu 16.06 and 18.04
    
    try update ansible version
    
    see https://codio.myjetbrains.com/youtrack/issue/codio-9995
    
    * try use python 3
    
    * update paths
    
    * package fixes
    
    * fix interpreter path
    
    * try up vnc
    use branch for now
    
    * indentation
    
    * check if 2
    
    * fix service path
    
    * absolute executable path
    reload daemon
    
    * absolute executable path
    
    * run from root
    
    * add enabled
    
    * openjdk-zulu-11
    
    * silent output
    update condition
    
    * lsb_release -c -s
    
    * fetch codename
    
    * fetch command status immediately after command
    
    * add missed package
    
    * remove second entry of python-cryptography
    
    * preparing for merge, use master

--- tools/ansible.sh
+++ tools/ansible.sh
@@ -3,28 +3,34 @@
 COOKBOOK_PATH=/tmp/codio_playbook
 BRANCH=master
 
+CODENAME=$(lsb_release -c -s)
+
+IS_TRUSTY() { [ "${CODENAME}" == "trusty" ]; }
+IS_XENIAL() { [ "${CODENAME}" == "xenial" ]; }
+IS_BIONIC() { [ "${CODENAME}" == "bionic" ]; }
+
 do_cmd()
 {
-	echo "Running [ $@ ]"
+    echo "Running [ $* ]"
     "$@"
     ret=$?
     if [[ $ret -eq 0 ]]
     then
-        echo "Successfully ran [ $@ ]"
+        echo "Successfully ran [ $* ]"
     else
-        echo "Error: Command [ $@ ] returned $ret"
-        echo "One more attempt for [ $@ ] after delay"
+        echo "Error: Command [ $* ] returned $ret"
+        echo "One more attempt for [ $* ] after delay"
 
         sleep 5
 
         "$@"
-	    ret=$?
-	    if [[ $ret -eq 0 ]]
-	    then
-	        echo "Successfully ran [ $@ ]"
-	    else
-	    	echo "Error: Command [ $@ ] returned $ret - Exiting"
-        	exit $ret
+        ret=$?
+        if [[ $ret -eq 0 ]]
+        then
+            echo "Successfully ran [ $* ]"
+        else
+            echo "Error: Command [ $* ] returned $ret - Exiting"
+            exit $ret
         fi
     fi
 }
@@ -36,17 +42,40 @@ download_playbook()
    curl -fsSL https://github.com/codio/install_software/archive/${BRANCH}.tar.gz  | tar zxf - -C ${COOKBOOK_PATH}
 }
 
-ansible --version | grep '2.2.0.0' 2> /dev/null
-is_ansible_right=$?
+if IS_TRUSTY; then
+    ansible --version | grep -q '2.2.0.0' 2> /dev/null
+    is_ansible_right=$?
+fi
+if IS_XENIAL; then
+    ansible --version | grep -q '2.7.5' 2> /dev/null
+    is_ansible_right=$?
+fi
+if IS_BIONIC; then
+    ansible --version | grep -q '2.7.5' 2> /dev/null
+    is_ansible_right=$?
+fi
 if [ $is_ansible_right -ne 0 ]; then
     do_cmd sudo apt-get update
-	do_cmd sudo apt-get -y install wget python python-support python-yaml python-httplib2 python-setuptools python-markupsafe python-jinja2 python-paramiko sshpass
-	do_cmd sudo wget -O /tmp/ansible.deb https://raw.githubusercontent.com/codio/install_software/master/tools/ansible_2.2.0.0-1ppa~trusty_all.deb
-	do_cmd sudo dpkg -i /tmp/ansible.deb
+    if IS_TRUSTY; then
+        do_cmd sudo apt-get -y install wget python python-support python-yaml python-httplib2 python-setuptools python-markupsafe python-jinja2 python-paramiko sshpass
+        do_cmd sudo wget -O /tmp/ansible.deb https://raw.githubusercontent.com/codio/install_software/${BRANCH}/tools/ansible_2.2.0.0-1ppa~trusty_all.deb
+    fi
+    if IS_XENIAL; then
+        do_cmd sudo apt-get -y install wget python python-cryptography python-crypto python-yaml python-httplib2 python-setuptools python-markupsafe python-jinja2 python-paramiko sshpass
+        do_cmd sudo wget -O /tmp/ansible.deb https://raw.githubusercontent.com/codio/install_software/${BRANCH}/tools/ansible_2.7.5-1ppa_xenial_all.deb
+    fi
+    if IS_BIONIC; then
+        do_cmd sudo apt-get -y install wget python python-crypto python-yaml python-httplib2 python-setuptools python-markupsafe python-jinja2 python-paramiko sshpass
+        do_cmd sudo wget -O /tmp/ansible.deb https://raw.githubusercontent.com/codio/install_software/${BRANCH}/tools/ansible_2.7.5-1ppa_bionic_all.deb
+    fi
+    do_cmd sudo dpkg -i /tmp/ansible.deb
     do_cmd sudo rm /tmp/ansible.deb
 fi
 
 download_playbook
 
-sudo ansible-playbook -v ${COOKBOOK_PATH}/install_software-${BRANCH}/$0/playbook.yaml
-
+if IS_TRUSTY; then
+    sudo ansible-playbook -v "${COOKBOOK_PATH}/install_software-${BRANCH}/$0/playbook.yaml"
+else
+    sudo ansible-playbook -v "${COOKBOOK_PATH}/install_software-${BRANCH}/$0/playbook.yaml" -e 'ansible_python_interpreter=/usr/bin/python2'
+fi
