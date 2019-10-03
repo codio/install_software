a9c68ebc96c9939e3423b4f1c984e7c41662822a
Maksim Kraev <maxim.kraev@gmail.com>
2019-10-03 12:59:42 +0100

    update default path for ungit

--- ungit/install.sh
+++ ungit/install.sh
@@ -4,4 +4,4 @@ bash -c "$(curl -fsSL https://raw.github.com/codio/install_software/master/tools
 
 echo
 echo "Add the line to you preview configuration to access ungit from the Menu"
-echo '   "ungit": "https://{{domain8000}}/"'
+echo '   "ungit": "https://{{domain8000}}/#/repository?path=%2Fhome%2Fcodio%2Fworkspace"'
