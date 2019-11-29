9a44137d827db35d314a344d574be06d3c3fe92f
Maksim Kraev <mkraev@codio.com>
2019-11-29 10:48:01 +0300

    fix error if  runtime folder exists

--- xserver/files/codio-xserver.sh
+++ xserver/files/codio-xserver.sh
@@ -2,5 +2,5 @@ export DISPLAY=:0
 export XAUTHORITY=/tmp/codio.auth
 export XDG_RUNTIME_DIR=/tmp/runtime-codio
 
-mkdir '/tmp/runtime-codio'
+mkdir -p '/tmp/runtime-codio'
 chmod 700 '/tmp/runtime-codio'
