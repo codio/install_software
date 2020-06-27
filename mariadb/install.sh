f097c35b176263ce40eb972da854d6ece2862fe4
maximka <maxim.kraev@gmail.com>
2020-06-27 12:53:49 +0000

    fix path

--- mariadb/install.sh
+++ mariadb/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bash -c "$(curl -fsSL https://raw.github.com/codio/install_software/master/tools/ansible.sh)" mysql
+bash -c "$(curl -fsSL https://raw.github.com/codio/install_software/master/tools/ansible.sh)" mariadb
 
 echo "Mysql password root user password is 'codio'"
