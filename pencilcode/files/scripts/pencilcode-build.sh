2eb066b7fa241644f31d1a7ff944df7f71544e6b
Andrey Nikitin <anikitin@codio.com>
2021-04-16 14:29:52 +0300

    try execute grunt tasks separately

--- pencilcode/files/scripts/pencilcode-build.sh
+++ pencilcode/files/scripts/pencilcode-build.sh
@@ -2,4 +2,7 @@
 
 cd /home/codio/pencilcode
 
-npm --max_old_space_size=512 run build
+npm --max_old_space_size=256 run test -- browserify:dist
+npm --max_old_space_size=256 run test -- uglify
+npm --max_old_space_size=256 run test -- less
+npm --max_old_space_size=256 run test -- builddate
