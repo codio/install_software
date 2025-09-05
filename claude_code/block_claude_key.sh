18e8edf3aabe89c252012a33ce9426dee7bd3a05
Max Kraev <maxim.kraev@gmail.com>
2025-09-05 11:06:12 +0000

    fixes

--- claude_code/block_claude_key.sh
+++ claude_code/block_claude_key.sh
@@ -7,15 +7,15 @@ fi
 LAST_20_CHARS="${ANTHROPIC_API_KEY: -20}"
 
 # Check if the value already exists in the array to avoid duplicates
-EXISTS=$(jq -r --arg val "$LAST_20_CHARS" '.customApiResponses.rejected[]? | select(. == $val)' "$JSON_FILE" 2>/dev/null)
+EXISTS=$(jq -r --arg val "$LAST_20_CHARS" '.customApiKeyResponses.rejected[]? | select(. == $val)' "$JSON_FILE" 2>/dev/null)
 
 if [ -n "$EXISTS" ]; then
   exit 0
 fi
 
-# Add the value to the customApiResponses.rejected array
+# Add the value to the customApiKeyResponses.rejected array
 # This will create the nested structure if it doesn't exist
 jq --arg val "$LAST_20_CHARS" '
-    .customApiResponses.rejected //= [] | 
-    .customApiResponses.rejected += [$val]
+    .customApiKeyResponses.rejected //= [] | 
+    .customApiKeyResponses.rejected += [$val]
 ' "$JSON_FILE" >> "${JSON_FILE}.tmp" && mv "${JSON_FILE}.tmp" "$JSON_FILE"
